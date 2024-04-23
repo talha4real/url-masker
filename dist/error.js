eb8ebb27b8c359a44bd0865d7a3e207ea424e2c7
talha4real <talhabutt993@gmail.com>
2024-04-23 19:27:15 +0100

    removed extra files

--- dist/error.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setErrorMap = void 0;
-function setErrorMap(map) {
-}
-exports.setErrorMap = setErrorMap;
--- dist/helpers/util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateUniqueId = void 0;
-function generateUniqueId() {
-    return Math.random().toString(36).substring(7);
-}
-exports.generateUniqueId = generateUniqueId;
